93981002ba55989994cc6927e3f7977ec315fde3
liuxin <liuxin@qwe.com>
2019-04-30 15:27:24 +0800

    aaabbbccc

--- git_test/src/com/itheima/FirstClass.java
+++ git_test/src/com/itheima/FirstClass.java
@@ -1,7 +1,8 @@
-package com.itheima;
+package git_test.src.com.itheima;
 
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("bbb");
+        System.out.println("ccc");
     }
 }
